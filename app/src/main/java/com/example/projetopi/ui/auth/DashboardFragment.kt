c509cbd7e2cba23ebb8644e5a0496f3297f16415
FabinBH <fabiodepaulo2008@mail.com>
2025-08-28 10:49:06 -0300

    Dashboard working

--- app/src/main/java/com/example/projetopi/ui/auth/DashboardFragment.kt
+++ app/src/main/java/com/example/projetopi/ui/auth/DashboardFragment.kt
@@ -5,14 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.projetopi.R
+import com.example.projetopi.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
+
+    private var _binding: FragmentDashboardBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    ): View {
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.buttonCriarConta.setOnClickListener {
+            findNavController().navigate(R.id.signupFragment2)
+        }
+        binding.buttonLogin.setOnClickListener {
+            findNavController().navigate(R.id.loginFragment3)
+        }
     }
 }
