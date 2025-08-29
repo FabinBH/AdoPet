73636ad00dd80fe2e9f624ee9cc12e38e0f9a9b2
20231TIIMI0111 <fabiodepaulo2008@gmail.com>
2025-08-29 08:00:26 -0300

    Update Login page navigation

--- app/src/main/java/com/example/projetopi/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/projetopi/ui/auth/LoginFragment.kt
@@ -5,14 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.projetopi.R
+import com.example.projetopi.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.imageButton.setOnClickListener {
+            findNavController().navigate(R.id.dashboardFragment)
+        }
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.adoptionFragment)
+        }
+        binding.criarconta.setOnClickListener {
+            findNavController().navigate(R.id.signupFragment2)
+        }
+        // Tela de "Esqueceu a senha" precisa ser criada
+        //binding.textView3.setOnClickListener {
+        //    findNavController().navigate(R.id.forgotPasswordFragment)
+        //}
     }
 }
