4807163764309b75b0cca6fbe3ff9b46be9d323d
Fabio Ferreira <fabiodepaulo2008@gmail.com>
2025-09-03 18:48:31 -0300

    Add filter fragment

---
+++ app/src/main/java/com/example/projetopi/ui/util/FilterFragment.kt
@@ -0,0 +1,12 @@
+package com.example.projetopi.ui.util
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.projetopi.R
+
+class FilterFragment : Fragment() {
+
+}
