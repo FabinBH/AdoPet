bf6d09b222bea985e3685b5160b5304a36dabefc
20231TIIMI0111 <fabiodepaulo2008@gmail.com>
2025-09-05 08:53:10 -0300

    Update menu view

--- app/src/main/java/com/example/projetopi/ui/auth/AdoptionFragment.kt
+++ app/src/main/java/com/example/projetopi/ui/auth/AdoptionFragment.kt
@@ -2,11 +2,13 @@ package com.example.projetopi.ui.auth
 
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.navigation.fragment.findNavController
 import com.example.projetopi.R
 import com.example.projetopi.databinding.FragmentAdoptionBinding
@@ -29,12 +31,15 @@ class AdoptionFragment : Fragment() {
         _binding = null
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val bottomNav = view.findViewById<BottomNavigationView>(R.id.bottom_nav)
         val specialItem = bottomNav.menu.findItem(R.id.nav_register)
         val colorStateList = ColorStateList.valueOf(Color.parseColor("#FE8C00"))
+
+        specialItem.iconTintList = colorStateList
 
         initListeners()
     }
