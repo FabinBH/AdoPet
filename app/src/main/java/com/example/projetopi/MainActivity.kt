035f719d971d0d9edd42754a43898ffe13c0fef4
20231TIIMI0111 <fabiodepaulo2008@gmail.com>
2025-09-12 08:25:10 -0300

    Menu finished

--- app/src/main/java/com/example/projetopi/MainActivity.kt
+++ app/src/main/java/com/example/projetopi/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.projetopi
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        bottomNav.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.adoptionFragment,
+                R.id.ongFragment,
+                R.id.chatFragment,
+                R.id.socialFragment,
+                R.id.profileFragment -> bottomNav.visibility = View.VISIBLE
+                else -> bottomNav.visibility = View.GONE
+            }
+        }
     }
 }
