c317b5f8023b0c20d7101ac6b57e3cc5bdc0984e
20231TIIMI0111 <fabiodepaulo2008@gmail.com>
2025-09-18 08:40:09 -0300

    Add Choose Fragment to update with RecyclerView

---
+++ app/src/main/java/com/example/projetopi/ui/ChooseFragment.kt
@@ -0,0 +1,46 @@
+package com.example.projetopi.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.projetopi.databinding.FragmentChooseBinding
+import com.example.projetopi.ui.adapter.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+
+class ChooseFragment : Fragment() {
+    private var _binding: FragmentChooseBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentChooseBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initTabs()
+    }
+
+    private fun initTabs() {
+        val pageAdapter = ViewPagerAdapter(requireActivity())
+        binding.viewPager.adapter = pageAdapter
+
+        pageAdapter.addFragment(OngFragment(), 1)
+
+        binding.viewPager.offscreenPageLimit = pageAdapter.itemCount
+
+        TabLayoutMediator(binding.tabs, binding.viewPager) {
+            tab, position -> tab.text = getString(pageAdapter.getTitle(position))
+        }.attach()
+    }
+}
