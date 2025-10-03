1b7cd681c44c0bb8d5bd9eb5be021a6a7136f1e7
Fabio Ferreira <fabiodepaulo2008@gmail.com>
2025-10-03 16:48:19 -0300

    Update Recover Password page

---
+++ app/src/main/java/com/example/projetopi/ui/auth/RecoverPasswordFragment.kt
@@ -0,0 +1,39 @@
+package com.example.projetopi.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.projetopi.R
+import com.example.projetopi.databinding.FragmentRecoverPasswordBinding
+
+class RecoverPasswordFragment : Fragment() {
+    private var _binding: FragmentRecoverPasswordBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRecoverPasswordBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.imageView.setOnClickListener {
+            findNavController().navigate(R.id.loginFragment3)
+        }
+    }
+}
