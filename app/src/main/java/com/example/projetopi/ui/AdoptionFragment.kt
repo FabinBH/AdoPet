f019df09646bc547698b288ba1222b5f3dc2d8c7
20231TIIMI0111 <fabiodepaulo2008@gmail.com>
2025-10-16 09:01:43 -0300

    Add chat navigation

--- app/src/main/java/com/example/projetopi/ui/AdoptionFragment.kt
+++ app/src/main/java/com/example/projetopi/ui/AdoptionFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projetopi.R
@@ -59,5 +60,13 @@ class AdoptionFragment : Fragment() {
         )
 
         adapter.submitList(pets)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnChat.setOnClickListener {
+            findNavController().navigate(R.id.action_adoptionFragment_to_chatFragment)
+        }
     }
 }
