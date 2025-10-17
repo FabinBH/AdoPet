b3bcc5dd16d2731cbf75011f2b18cc5cd1366dc1
20231TIIMI0111 <fabiodepaulo2008@gmail.com>
2025-10-17 08:24:41 -0300

    Update authentication

--- app/src/main/java/com/example/projetopi/ui/auth/SignupFragment.kt
+++ app/src/main/java/com/example/projetopi/ui/auth/SignupFragment.kt
@@ -5,19 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.projetopi.R
 import com.example.projetopi.databinding.FragmentSignupBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class SignupFragment : Fragment() {
     private var _binding: FragmentSignupBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSignupBinding.inflate(inflater, container, false)
+        auth = Firebase.auth
         return binding.root
     }
 
@@ -28,7 +34,32 @@ class SignupFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload()
+        }
+
         initListeners()
+    }
+
+    private fun reload() {  }
+
+    private fun signUp(email: String, password: String) {
+        try {
+            val auth = FirebaseAuth.getInstance()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        findNavController().navigate(R.id.action_global_adoptionFragment)
+                    } else {
+                        Toast.makeText(requireContext(), task.exception?.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+        } catch (e: Exception) {
+            Toast.makeText(requireContext(), e.message.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun initListeners() {
